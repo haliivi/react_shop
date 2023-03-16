ab7ed179a662633b38406ca3d1814c0020b0ccdc
a.sotnichenko <haliivi@mail.ru>
2023-03-16 13:12:01 +0700

    Start project.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Footer} from './layouts/Footer'
+import {Header} from './layouts/Header'
+import {Shop} from './layouts/Shop'
+import {API_KEY, API_URL} from './config'
+
+function App() {
+  return (
+    <>
+        <Header />
+        <Shop />
+        <Footer />
+    </>
+  );
+}
+
+export default App;
